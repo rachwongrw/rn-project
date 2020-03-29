85f4cb02fd3543053a659b9d99b2db38bf035ae2
Rachel Wong <rwrachelwong@gmail.com>
2020-03-29 12:51:22 -0700

    convert useState to useReducer fn

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,46 +1,50 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { StyleSheet, View } from 'react-native'
 import ColourCounter from '../components/ColourCounter'
 
-const INCREMENT = 10 // capitalize this constant as to show it's a special variable
+const INCREMENT = 15 // capitalize this constant as to show it's a special variable
+
+const reducer = (state, action) => {
+  // state = { red: number, green: number, blue: number }
+  // action = { colourToChange: 'red' || 'green' || 'blue', amount:  15 || -15 }
+  switch(action.colourToChange) {
+    case 'red':
+      return state.red + action.amount > 255 || state.red + action.amount < 0 ?
+        state
+        : { ...state, red: state.red + action.amount }
+    case 'green':
+      return state.green + action.amount > 255 || state.green + action.amount < 0 ?
+      state
+      : { ...state, green: state.green + action.amount }
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0 ?
+      state
+      : { ...state, blue: state.blue + action.amount }
+    default:
+      return state
+  }
+}
 
 const SquareScreen = () => {
-  const [ red, setRed ] = useState(0) // useState is a React Hook - not actually changing the state
-  const [ green, setGreen ] = useState(0)
-  const [ blue, setBlue ] = useState(0)
-
-  const setColour = (colour, change) => {
-    switch (colour) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change)
-        return
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-        return 
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-        return 
-      default:
-        return
-    }
-  }
-
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
+  // dispatch i.e run my reducer
+  const { red, green, blue } = state 
   return (
     <View>
       <ColourCounter
         colour="Red" 
-        onIncrease={() => setColour('red',  INCREMENT)} 
-        onDecrease={() => setColour('red', -1 * INCREMENT)}
+        onIncrease={() => dispatch({colourToChange: 'red', amount: INCREMENT})} 
+        onDecrease={() => dispatch({colourToChange: 'red', amount: -1 * INCREMENT})}
       />
       <ColourCounter 
         colour="Green"
-        onIncrease={() => setColour('green', INCREMENT)} 
-        onDecrease={() => setColour('green', -1 * INCREMENT)} 
+        onIncrease={() => dispatch({colourToChange: 'green', amount: INCREMENT})} 
+        onDecrease={() => dispatch({colourToChange: 'green', amount: -1 * INCREMENT})} 
       />
       <ColourCounter 
         colour="Blue"
-        onIncrease={() => setColour('blue', INCREMENT)} 
-        onDecrease={() => setColour('blue', -1 * INCREMENT)} 
+        onIncrease={() => dispatch({colourToChange: 'blue', amount: INCREMENT})} 
+        onDecrease={() => dispatch({colourToChange: 'blue', amount: -1 * INCREMENT}) } 
       />
       <View style={{height:150, width:150, backgroundColor:`rgb(${red}, ${green}, ${blue})`}}/>
     </View>
